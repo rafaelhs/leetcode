8447e6ec733a186f227d33011bfcf2ff3e17233c
rafael <rhirata.s@gmail.com>
2023-12-05 16:49:42 -0300

    Added info

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-            System.out.println("Hello World!");
+            //This project is just a place to store my Leet Code solutions.
     }
 }
