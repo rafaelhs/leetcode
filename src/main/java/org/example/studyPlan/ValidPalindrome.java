69e4a829fc83a9f315a17cd59a906f9f2ea2be6a
rafael <rhirata.s@gmail.com>
2023-12-15 14:31:26 -0300

    24 - Valid Palindrome

---
+++ src/main/java/org/example/studyPlan/ValidPalindrome.java
@@ -0,0 +1,12 @@
+package org.example.studyPlan;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0, right = s.length()-1;
+        while(left < right) {
+            if(s.charAt(left++) != s.charAt(right--)) return false;
+        }
+        return true;
+    }
+}
